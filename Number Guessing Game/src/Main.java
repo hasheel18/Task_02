962ad952bb144352857efc1e8d56d86a565bb846
Dhanusha Hasheel <dhanushahasheeluni@gmail.com>
2025-11-08 22:43:09 +0530

    Committed 4th task

---
+++ Number Guessing Game/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public  class Main {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        int guess;
+        int attempts = 0;
+        int min = 1;
+        int max = 100;
+        int randomNumber = random.nextInt(min, max + 1);
+
+        System.out.println("Number Guessing Game");
+        System.out.printf("Guess a number between %d-%d\n", min, max);
+
+        do{
+            System.out.print("Enter a guess : ");
+            guess = scanner.nextInt();
+            attempts++;
+
+            if(guess < randomNumber){
+                System.out.println("Too low, Try again!");
+            }else if(guess > randomNumber){
+                System.out.println("Too high, Try again!");
+            }else {
+                System.out.println("Correct!, The number was " + randomNumber);
+                System.out.println("Number of attempts : " + attempts);
+            }
+
+        }while(guess != randomNumber);
+
+        System.out.println("You have won");
+        scanner.close();
+
+    }
+}
